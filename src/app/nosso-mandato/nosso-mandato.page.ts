2c38bfd3473c1bc44d294d6d545cc88899964ca8
Víctor Romário <victor.romario.seriado@gmail.com>
2021-03-17 22:17:31 -0300

    feat: add function nextPage() in 'Nosso Mandato'

--- src/app/nosso-mandato/nosso-mandato.page.ts
+++ src/app/nosso-mandato/nosso-mandato.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from  '@ionic/angular';
 
 @Component({
   selector: 'app-nosso-mandato',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NossoMandatoPage implements OnInit {
 
-  constructor() { }
+  constructor(private nav:NavController) { }
 
   ngOnInit() {
+  }
+
+  proximo(values:any) {
+    this.nav.navigateForward('/'+values);
   }
 
 }
