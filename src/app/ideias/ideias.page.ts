0aae7f36b3dd5dab49ec0e9e08fc6c46d8d266d3
Víctor Romário <victor.romario.seriado@gmail.com>
2021-03-18 22:47:39 -0300

    feat: create page '43 ideias para Salvador'

---
+++ src/app/ideias/ideias.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from  '@ionic/angular';
+
+@Component({
+  selector: 'app-ideias',
+  templateUrl: './ideias.page.html',
+  styleUrls: ['./ideias.page.scss'],
+})
+export class IdeiasPage implements OnInit {
+
+  constructor(private nav:NavController) { }
+
+  ngOnInit() {
+  }
+
+  proximo(values:any) {
+    this.nav.navigateForward('/'+values);
+  }
+
+}
