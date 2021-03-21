deaa2614fa49ef45519eb846d7295e9bd07cac95
Víctor Romário <victor.romario.seriado@gmail.com>
2021-03-20 22:18:04 -0300

    feat: add function back page

--- src/app/ideias-ods3/ideias-ods3.page.ts
+++ src/app/ideias-ods3/ideias-ods3.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from  '@ionic/angular';
 
 @Component({
   selector: 'app-ideias-ods3',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IdeiasOds3Page implements OnInit {
 
-  constructor() { }
+  constructor(private nav:NavController) { }
 
   ngOnInit() {
+  }
+
+  proximo(values:any) {
+    console.log(values)
+    this.nav.navigateForward('/'+values);
   }
 
 }
