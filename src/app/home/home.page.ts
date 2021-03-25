41bae850ff95410774ac350be53826958ea2f538
Víctor Romário <victor.romario.seriado@gmail.com>
2021-03-24 21:22:25 -0300

    feat: add modal 'Post' in 'Home'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from  '@ionic/angular';
+import { ModalController } from '@ionic/angular';
+import { PostPage } from '../modals/post/post.page';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +10,7 @@ import { NavController } from  '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private nav:NavController) {}
+  constructor(private nav:NavController, public modalController: ModalController) {}
 
   slideOpts = {
     initialSlide: 0,
@@ -17,6 +19,19 @@ export class HomePage {
 
   proximo(values:any) {
     this.nav.navigateForward('/'+values);
+  }
+
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: PostPage,
+      //cssClass: 'my-custom-class',
+      componentProps: {
+        'firstName': 'Douglas',
+        'lastName': 'Adams',
+        'middleInitial': 'N'
+      }
+    });
+    return await modal.present();
   }
 
 }
