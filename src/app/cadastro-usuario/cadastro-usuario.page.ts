8eba76e52a0a54bd8fa312c4d988328ce4d15b98
Víctor Romário <victor.romario.seriado@gmail.com>
2021-04-11 17:53:36 -0300

    feat: get value from form of 'cadastro de usuários'

--- src/app/cadastro-usuario/cadastro-usuario.page.ts
+++ src/app/cadastro-usuario/cadastro-usuario.page.ts
@@ -9,6 +9,19 @@ import { FirebaseService } from '../services/firebase.service';
 })
 export class CadastroUsuarioPage implements OnInit {
 
+  private name: string
+  private birthDate: string
+  private telephone: string
+  private address: string
+  private district: string
+  private email: string
+  private socialNetwork: string
+  private andreVoter: string
+  private participantAmbassadorNetwork: string
+  private password: string
+  private confirmPassword: string
+
+
   constructor(private nav:NavController, private firebaseService: FirebaseService) { }
 
   ngOnInit() {
@@ -16,6 +29,21 @@ export class CadastroUsuarioPage implements OnInit {
 
   proximo(values:any) {
     this.nav.navigateForward('/'+values)
+  }
+
+  registerUser() {
+    console.log('Register user')
+    console.log('name', this.name);
+    console.log('birthDate', this.birthDate);
+    console.log('telephone', this.telephone);
+    console.log('address', this.address);
+    console.log('district', this.district);
+    console.log('email', this.email);
+    console.log('socialNetwork', this.socialNetwork);
+    console.log('andreVoter', this.andreVoter);
+    console.log('participantAmbassadorNetwork', this.participantAmbassadorNetwork);
+    console.log('password', this.password);
+    console.log('confirmPassword', this.confirmPassword);
   }
 
 }
