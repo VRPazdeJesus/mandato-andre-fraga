64dfc861c84683344f38dc89df8437b6d2578c35
Víctor Romário <victor.romario.seriado@gmail.com>
2021-04-14 23:38:31 -0300

    feat: create page 'fazer-denuncias'

--- src/app/fazer-denuncia/fazer-denuncia.page.ts
+++ src/app/fazer-denuncia/fazer-denuncia.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { NavController } from  '@ionic/angular'
 
 @Component({
   selector: 'app-fazer-denuncia',
@@ -7,9 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FazerDenunciaPage implements OnInit {
 
-  constructor() { }
+  tituloDoProjeto: string
+  categoriaDoProjeto: string
+  resumoDaSolicitacao: string
+  categories = [
+    'Acessibilidade e Cidadania',
+    'Administração e Gestão de Pessoa',
+    'Assistencia ao Animal',
+    'Comunicação',
+    'Cultura',
+    'Datas Comemorativas',
+    'Denominação de Logadouro',
+    'Desenvolvimento Econômico e Social',
+    'Direitos da Mulher',
+    'Direito e Proteção Idoso',
+    'Direitos Humanos e Justiça',
+    'Educação',
+    'Esporte e Lazer',
+    'Finanças e Orçamento',
+    'Frente Parlamentar',
+    'Honraria',
+    'Meio Ambiente',
+    'Planejamento Urbano',
+    'Previdência Social',
+    'Reparação',
+    'Saúde',
+    'Segurança / Combate a Violência',
+    'Serviço Público',
+    'Trasnporte e Trânsito',
+    'Turismo e Desporto',
+    'Utilidade Pública',
+    'Outros'
+  ]
+  bairroDoProjeto: string
+  entidadeDoProjeto: string
+  valorDoProjeto: string
+  fotoDenuncia: string
+
+  constructor(private nav:NavController) { 
+    this.tituloDoProjeto = ''
+    this.categoriaDoProjeto = 'Selecione a categoria'
+    this.resumoDaSolicitacao = ''
+    this.bairroDoProjeto = ''
+    this.entidadeDoProjeto = ''
+    this.valorDoProjeto = ''
+  }
 
   ngOnInit() {
+  }
+
+  proximo(values:any) {
+    this.nav.navigateForward('/'+values)
+  }
+
+  sendProposal() {
+    this.tituloDoProjeto = ''
+    console.log("Enviando proposta")
+    this.categoriaDoProjeto = 'Selecione a categoria'
+    this.resumoDaSolicitacao = ''
+    this.bairroDoProjeto = ''
+    this.entidadeDoProjeto = ''
+    this.valorDoProjeto = ''
+    this.proximo('resposta-de-envio')
   }
 
 }
